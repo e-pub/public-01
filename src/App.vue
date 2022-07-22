87269462d593495cbebec61d22022f84f7efe27c
GRACE\tecar <gpubmenu@gmail.com>
2022-07-23 01:54:55 +0900

    [main] app원본 유지/header 네이밍 변경

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <headerTop msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import headerTop from './components/headerTop.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    headerTop
   }
 }
 </script>
 
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+* {margin:0; padding:0;}
 </style>
---
+++ src/components/headerTop.vue
@@ -0,0 +1,11 @@
+<template>
+  <header>
+      <h1>LOGO</h1>
+  </header>
+</template>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+header {height:80px; line-height:80px; padding:0 20px; display:flex; background-color:#0158a8;}
+h1 {color:#fff;}
+</style>
