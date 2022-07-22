c831d93dc3023fdf2fe9eb3246939df550b3f888
GRACE\tecar <gpubmenu@gmail.com>
2022-07-23 02:12:40 +0900

    [pub-01] header변경

--- src/App.vue
+++ src/App.vue
@@ -16,4 +16,9 @@ export default {
 
 <style>
 * {margin:0; padding:0;}
+ul {display:inline-block;}
+li {float:left; list-style:none;}
+.right {float:right !important;}
+.bg {background:red;}
+.bgb {background:blue;}
 </style>
--- src/components/headerTop.vue
+++ src/components/headerTop.vue
@@ -1,11 +1,19 @@
 <template>
   <header>
-      <h1>LOGO:서울시청</h1>
+      <h1 class="logo">LOGO:서울시청</h1>
+      <ul class="top-menu-right right">
+        <li>menu-1</li>
+        <li>menu-2</li>
+        <li>menu-3</li>
+        <li>menu-4</li>
+      </ul><!--//top-menu-right/right-->
   </header>
 </template>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-header {height:80px; line-height:80px; padding:0 20px; display:flex; background-color:#0158a8;}
-h1 {color:#fff;}
+header {display:block; height:80px; line-height:80px; padding:0 20px; background-color:#0158a8;}
+.logo {display:inline-block; color:#fff;}
+.top-menu-right {float:right !important;}
+.top-menu-right li {color:yellow; font-size:16px; margin-left:30px;}
 </style>
